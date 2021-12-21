d2c81efbc06f1f97957aa871db423633fce88d9f
Miqo95 <gmiqael@bk.ru>
2021-12-21 17:49:51 +0400

    finishOneDay

--- coc-app/src/App.js
+++ coc-app/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages";
+import SingleCoctale from "./pages/SingleCoctale";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cocktail/:id" element={<SingleCoctale />} />
+      </Routes>
     </div>
   );
 }
