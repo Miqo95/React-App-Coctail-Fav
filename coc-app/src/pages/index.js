8b0d5a1eb625b46ca2a43deb7b8dbd7319e50d6a
Miqo95 <gmiqael@bk.ru>
2021-12-21 17:11:30 +0400

    work2OneDay

---
+++ coc-app/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+      {/* Search input */}
+      {/* Coctail List */}
+    </div>
+  );
+};
+
+export default Home;
